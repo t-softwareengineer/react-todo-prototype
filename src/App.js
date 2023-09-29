363302fd23e26f7439ca9f8007c9fcdf497d8fa9
t-softwareengineer <t.softwareengineer@hotmail.com>
2023-09-30 00:05:20 +0100

    build: hardcoded dummy data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.scss';
 import NavigationBar from './components/NavigationBar';
 import Footer from './components/Footer';
 
 function App() {
+  // HARDCODED! - Dummy Data
+  const [todos, setTodos] = useState([
+    {
+      completed: false,
+      text: 'Meeting at 2',
+      id: 1
+    },
+    {
+      completed: false,
+      text: 'Drink Water!',
+      id: 2
+    },
+    {
+      completed: true,
+      text: 'Learn HTML',
+      id: 3
+    }
+  ]);
+  
   return (
     <>
       <NavigationBar />
