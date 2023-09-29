5d3d380c60e7f14a29ced9dbfa0d0587946d9959
t-softwareengineer <t.softwareengineer@hotmail.com>
2023-09-29 23:54:57 +0100

    build: navigation bar & footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+
+import { FaGithub } from "react-icons/fa6";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <small>Created by <FaGithub /> t-softwareengineer</small>
+    </footer>
+  );
+}
+
+export default Footer;
