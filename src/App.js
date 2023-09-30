1a80ef61e838a02e30080976d720219650ae165a
t-softwareengineer <t.softwareengineer@hotmail.com>
2023-09-30 21:00:24 +0100

    feat(api): create, delete, completed todo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.scss';
 import NavigationBar from './components/NavigationBar';
 import Footer from './components/Footer';
+import TodoList from './components/TodoList';
 
 function App() {
   // HARDCODED! - Dummy Data
@@ -22,10 +23,28 @@ function App() {
       id: 3
     }
   ]);
-  
+
+  // Create New Todo
+  const createTodo = (todo) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTodo = { id, ...todo };
+    setTodos([...todos, newTodo]);
+  }
+
+  // Delete Todo
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  // Toggle Completed
+  const toggleCompleted = (id) => {
+    setTodos(todos.map((todo) => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
+  }
+
   return (
     <>
       <NavigationBar />
+      <TodoList todos={todos} onDelete={deleteTodo} onToggle={toggleCompleted} />
       <Footer />
     </>
   );
